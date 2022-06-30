9f53feb8a6e6e907646431de5fc4d518a0979ea2
Aiden <62087606+long13820@users.noreply.github.com>
2022-07-01 00:37:59 +0700

    Get API Homepage

---
+++ .history/src/App_20220630180026.js
@@ -0,0 +1,35 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import React from ''
+import About from './components/About/About';
+import Cart from './components/Cart/Cart';
+import Home from './components/Home/Home';
+import Product from './components/Product/Product';
+import Shop from './components/Shop/Shop';
+import Title from './components/Title/Title';
+import Footer from './pages/Footer/Footer';
+import Header from './pages/Header/Header';
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className='d-flex flex-column m-height-100'>
+        <Header/>
+        
+        <Title title="About"/>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/shop' element={<Shop/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/shop/1' element={<Product/>} />
+          <Route path='/about' element={<About/>} />
+        </Routes>
+
+        <Footer/>
+
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
